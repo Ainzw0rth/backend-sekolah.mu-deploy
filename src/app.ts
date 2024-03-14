0430e7febe84c79e0be8b9c937322bce1d48baa1
naufal <89512179+nomsf@users.noreply.github.com>
2024-03-14 09:57:02 +0700

    migrate to typescript, restructure folder

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// // available routes
+// var indexRouter = require('./routes/index');
+// var kontenRouter = require('./routes/konten');
+
+// var app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // explicitly stating the routes
+// app.use('/', indexRouter);
+// app.use('/konten', kontenRouter);
+
+// module.exports = app;
+
+import { Application, Request, Response, NextFunction } from 'express';
+var express = require('express');
+
+const app: Application = express();
+const port: number = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => { 
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
