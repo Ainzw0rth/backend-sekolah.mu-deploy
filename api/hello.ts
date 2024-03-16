611797726e162a024bcbdddb9c25dacd8cd210b6
louis <louiscaesapdf@gmail.com>
2024-03-16 12:32:43 +0700

    update vercel.json

---
+++ api/hello.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+const handler = (req: Request, res: Response): void => {
+  res.status(200).json({ message: 'Hello, World!' });
+};
+
+export default handler;
