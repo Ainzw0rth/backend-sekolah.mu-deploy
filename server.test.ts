d2656e99fe3f5648026e106a94680a14068b4ba3
louis <louiscaesapdf@gmail.com>
2024-03-16 11:24:47 +0700

    update vercel.json

--- server.test.ts
+++ server.test.ts
@@ -1,9 +1,18 @@
-const baseURL = 'https://backend-sekolah-mu-development.vercel.app?_vercel_share=OKwwAtmieiLCz43JiqcpJCwUrO9WReIA'
+describe('API Status Code Test', () => {
+  it('should return status code 200 and print the response body', async () => {
+    const url = 'https://backend-sekolah-mu-development.vercel.app?_vercel_share=OKwwAtmieiLCz43JiqcpJCwUrO9WReIA';
+    const headers = {
+      'Accept': 'application/json',
+      Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`, // Request JSON data
+      // Add more headers if needed
+    };
 
-test('fetches data from API', async () => {
-    const response = await fetch(baseURL);
-    const data = await response.json();
-    console.log(data)
-    // Assertion
-    expect(data).toEqual({ key: 'value' });
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers
+    });
+
+    console.log('Response Body:', await response.text());
+    expect(response.status).toBe(200);
+  });
 });
