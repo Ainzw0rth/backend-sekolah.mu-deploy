b69bb264caafb4d4560844a00e52cb109df72021
louis <louiscaesapdf@gmail.com>
2024-03-16 12:45:58 +0700

    update vercel.json

--- api/hello.ts
+++ api/hello.ts
@@ -1,14 +1,7 @@
 import { Request, Response } from 'express';
 
 const handler = (req: Request, res: Response): void => {
-    res.setHeader('Access-Control-Allow-Credentials', "true");
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    res.status(200).json({ message: 'Hello, World!' });
+  res.status(200).json({ message: 'Hello, World!' });
 };
 
 export default handler;
