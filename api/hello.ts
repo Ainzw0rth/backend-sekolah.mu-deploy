f6fc73959d0d3b407e09a4f6ebbb5ef059b3c09d
louis <louiscaesapdf@gmail.com>
2024-03-16 12:39:52 +0700

    update api

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
 
 const handler = (req: Request, res: Response): void => {
-  res.status(200).json({ message: 'Hello, World!' });
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    res.status(200).json({ message: 'Hello, World!' });
 };
 
 export default handler;
