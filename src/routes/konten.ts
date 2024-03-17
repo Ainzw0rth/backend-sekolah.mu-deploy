050833b2c18a873f1d79c98034cd495339bc39ec
louis <louiscaesapdf@gmail.com>
2024-03-17 10:12:17 +0700

    update konten.ts

--- src/routes/konten.ts
+++ src/routes/konten.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, Router } from 'express';
-const kontenController = require('../controllers/konten_controller');
+import express, { Router } from 'express';
+import kontenController from '../controllers/konten_controller';
 const router: Router = express.Router();
 
 router.get('/', kontenController.getAll);
