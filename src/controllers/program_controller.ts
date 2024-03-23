b81760384353101bf3db42986a0fb58ccf175b3d
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-03-23 21:12:35 +0700

    feat: add endpoint get program by id guru

--- src/controllers/program_controller.ts
+++ src/controllers/program_controller.ts
@@ -4,6 +4,7 @@ import postgre from '../database';
 interface ProgramController {
     getAll: (req: Request, res: Response) => Promise<void>;
     getById: (req: Request, res: Response) => Promise<void>;
+    getByGuru: (req: Request, res: Response) => Promise<void>;
 }
 
 const programController: ProgramController = {
@@ -22,7 +23,15 @@ const programController: ProgramController = {
         } catch (error) {
             res.json({msg: error.msg})
         }
-    }
+    },
+    getByGuru: async (req, res) => {
+        try {
+            const { rows } = await postgre.query("SELECT * from program LEFT JOIN kelas ON kelas.program_id = program.program_id WHERE id_program = $1", [req.params.id]);
+            res.json({msg: "OK", data: rows})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },    
 } 
 
 export default programController;
--- src/routes/program.ts
+++ src/routes/program.ts
@@ -3,6 +3,7 @@ import programController from '../controllers/program_controller';
 const router: Router = express.Router();
 
 router.get('/', programController.getAll);
+router.get('/guru/:id', programController.getByGuru);
 router.get('/:id', programController.getById);
 
 export default router;
