f1bdfff7367013221dc5b78fd99c62b6439ed14c
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-03-23 23:57:57 +0700

    fix: idGuru type in kegiatan by guru

--- src/controllers/kegiatan_controller.ts
+++ src/controllers/kegiatan_controller.ts
@@ -64,26 +64,23 @@ const kegiatanController: KegiatanController = {
             if (!idGuru) {
                 res.json({msg: "ID Guru is required"});
                 return;
-            } else if (idGuru && (typeof idGuru !== 'number' || isNaN(idGuru))) {
+            } else if (idGuru && (typeof idGuru !== 'number' || isNaN(parseInt(idGuru)))) {
                 res.json({msg: "ID must be a number"});
                 return;
             }
             
-            let query = `
-                SELECT kegiatan.id_kegiatan, nama_kegiatan, id_guru, id_jadwal, tanggal, waktu, lokasi, id_topik, id_kelas
-                FROM kegiatan JOIN jadwal ON kegiatan.id_kegiatan = jadwal.id_kegiatan
-                WHERE id_guru = $1`;
-
-            const params = [idGuru];
-
-            if (dateString) {
-                query += ` AND tanggal = $2;`;
-                params.push(dateString);
-            }
-
-            const { rows } = await postgre.query(query, params);
-
+            const { rows } = await postgre.query(`
+                SELECT kegiatan.id_kegiatan, nama_kegiatan, nama_kelas, nama_program, nama_topik, tanggal, waktu
+                FROM kegiatan 
+                LEFT JOIN jadwal ON kegiatan.id_kegiatan = jadwal.id_kegiatan
+                LEFT JOIN kelas ON jadwal.id_kelas = kelas.id_kelas
+                LEFT JOIN topik ON topik.id_topik = kegiatan.id_topik
+                LEFT JOIN program ON topik.id_program = program.id_program
+                WHERE id_guru = $1 AND ($2 IS NULL OR tanggal = $2);
+            `, [parseInt(idGuru), dateString]);
+        
             res.json({msg: "OK", data: rows});
+        
         } catch (error) {
             res.json({msg: error.message});
         }
