0185fc48560e5728a37126276d85e1b11b2a9249
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-03-23 20:15:24 +0700

    feat: add instruksi guru route

--- src/routes/kegiatan.ts
+++ src/routes/kegiatan.ts
@@ -5,5 +5,6 @@ const router: Router = express.Router();
 router.get('/', kegiatanController.getAll);
 router.get('/:id', kegiatanController.getById);
 router.get('/instruksi', kegiatanController.getInstruksi);
+router.get('/instruksi-guru/:id', kegiatanController.getInstruksiGuru);
 
 export default router;
