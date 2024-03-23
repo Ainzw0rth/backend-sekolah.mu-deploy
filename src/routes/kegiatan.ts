c9d27485b654056b05fa9fff455ddfe08ede409f
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-03-23 20:20:27 +0700

    fix: route kegiatan

--- src/routes/kegiatan.ts
+++ src/routes/kegiatan.ts
@@ -3,8 +3,8 @@ import kegiatanController from '../controllers/kegiatan_controller';
 const router: Router = express.Router();
 
 router.get('/', kegiatanController.getAll);
-router.get('/:id', kegiatanController.getById);
 router.get('/instruksi', kegiatanController.getInstruksi);
 router.get('/instruksi-guru/:id', kegiatanController.getInstruksiGuru);
+router.get('/:id', kegiatanController.getById);
 
 export default router;
