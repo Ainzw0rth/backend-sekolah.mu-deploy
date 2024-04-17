f90a3cfa38541456754e631784e0d13e8e240dd3
louis <louiscaesapdf@gmail.com>
2024-04-17 16:24:13 +0700

    restore getguru and getid konten

--- src/controllers/kegiatan_controller.ts
+++ src/controllers/kegiatan_controller.ts
@@ -4,6 +4,8 @@ import postgre from '../database';
 interface KegiatanController {
     getAll: (req: Request, res: Response) => Promise<void>;
     getById: (req: Request, res: Response) => Promise<void>;
+    getInstruksi: (req: Request, res: Response) => Promise<void>;
+    getByGuru: (req: Request, res: Response) => Promise<void>;
 }
 
 const kegiatanController: KegiatanController = {
@@ -16,6 +18,41 @@ const kegiatanController: KegiatanController = {
         }
     },
     getById: async (req, res) => {
+        try {
+            const { rows } = await postgre.query("SELECT * from kegiatan WHERE id_kegiatan = $1", [req.params.id]);
+            res.json({msg: "OK", data: rows})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },
+    getInstruksi: async (req, res) => {
+        try {    
+            
+            if (!req.query.id) {
+                res.json({msg: "ID is required"});
+                return;
+            } else if (typeof req.query.id !== 'string' || isNaN(parseInt(req.query.id.toString()))) {
+                res.json({msg: "ID must be a number"});
+                return;
+            } else if (!req.query.type || (req.query.type !== "guru" && req.query.type !== "murid")) {
+                res.json({msg: "Type must be either 'guru' or 'murid'"});
+                return;
+            }
+    
+            let instruksiField;
+            if (req.query.type === "guru") {
+                instruksiField = "instruksi_guru";
+            } else {
+                instruksiField = "instruksi_murid";
+            }
+    
+            const { rows } = await postgre.query(`SELECT ${instruksiField} FROM kegiatan WHERE id_kegiatan = $1`, [req.query.id.toString()]);
+            res.json({msg: "OK", data: rows});
+        } catch (error) {
+            res.json({msg: error.msg});
+        }
+    },
+    getByGuru: async (req, res) => {
         try {
             const idGuru = req.query.id ? parseInt(req.query.id.toString()) : null;
             // TODO : filter by date
@@ -45,6 +82,6 @@ const kegiatanController: KegiatanController = {
         } catch (error) {
             res.json({msg: error.message});
         }
-    }
+    },     
 }
 export default kegiatanController;
--- src/routes/kegiatan.ts
+++ src/routes/kegiatan.ts
@@ -3,6 +3,8 @@ import kegiatanController from '../controllers/kegiatan_controller';
 const router: Router = express.Router();
 
 router.get('/', kegiatanController.getAll);
+router.get('/instruksi', kegiatanController.getInstruksi);
+router.get('/guru', kegiatanController.getByGuru);
 router.get('/:id', kegiatanController.getById);
 
 
