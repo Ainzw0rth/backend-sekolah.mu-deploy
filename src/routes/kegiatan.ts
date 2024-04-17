97a250efcbf7a96b92ef75038782734659312c3a
louis <louiscaesapdf@gmail.com>
2024-04-17 22:14:54 +0700

    restore back to normal

--- src/routes/kegiatan.ts
+++ src/routes/kegiatan.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import kegiatanController from '../controllers/kegiatan_controller';
 const router: Router = express.Router();
 
-router.get('/', kegiatanController.getByGuru);
+router.get('/', kegiatanController.getAll);
 router.get('/instruksi', kegiatanController.getInstruksi);
 router.get('/guru', kegiatanController.getByGuru);
 router.get('/:id', kegiatanController.getById);
