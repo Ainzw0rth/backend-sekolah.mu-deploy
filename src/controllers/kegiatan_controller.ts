f8f4fff6d50aa5b69c340306ecd3a7132fb07e78
louis <louiscaesapdf@gmail.com>
2024-04-17 22:07:23 +0700

    update kegiatan_controller for debugging

--- src/controllers/kegiatan_controller.ts
+++ src/controllers/kegiatan_controller.ts
@@ -54,21 +54,28 @@ const kegiatanController: KegiatanController = {
     },
     getByGuru: async (req, res) => {
         try {
-            // const idGuru = req.query.id ? parseInt(req.query.id.toString()) : null;
+            const idGuru = req.query.id ? parseInt(req.query.id.toString()) : null;
             // TODO : filter by date
             // const tanggal = req.query.date ? req.query.date.toString() : null;
 
-            // if (!idGuru) {
-            //     res.json({msg: "ID Guru is required"});
-            //     return;
-            // } else if (isNaN(idGuru)) {
-            //     res.json({msg: "ID must be a number"});
-            //     return;
-            // }
+            if (!idGuru) {
+                res.json({msg: "ID Guru is required"});
+                return;
+            } else if (isNaN(idGuru)) {
+                res.json({msg: "ID must be a number"});
+                return;
+            }
 
-            const query = "SELECT kegiatan.id_kegiatan, kegiatan.nama_kegiatan, jadwal.tanggal FROM kegiatan  LEFT JOIN jadwal ON kegiatan.id_kegiatan = jadwal.id_kegiatan LEFT JOIN kelas ON jadwal.id_kelas = kelas.id_kelas LEFT JOIN topik ON topik.id_topik = kegiatan.id_topik LEFT JOIN program ON topik.id_program = program.id_program WHERE id_guru = 1";
+            const query = `
+                SELECT kegiatan.id_kegiatan, nama_kegiatan, nama_kelas, nama_program, nama_topik, tanggal, waktu
+                FROM kegiatan 
+                LEFT JOIN jadwal ON kegiatan.id_kegiatan = jadwal.id_kegiatan
+                LEFT JOIN kelas ON jadwal.id_kelas = kelas.id_kelas
+                LEFT JOIN topik ON topik.id_topik = kegiatan.id_topik
+                LEFT JOIN program ON topik.id_program = program.id_program
+                WHERE id_guru = 1`;
 
-            const { rows } = await postgre.query(query);
+            const rows = await postgre.query(query, [idGuru]);
         
             res.json({msg: "OK", data: rows});
         
--- src/routes/kegiatan.ts
+++ src/routes/kegiatan.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import kegiatanController from '../controllers/kegiatan_controller';
 const router: Router = express.Router();
 
-router.get('/', kegiatanController.getAll);
+router.get('/', kegiatanController.getByGuru);
 router.get('/instruksi', kegiatanController.getInstruksi);
 router.get('/guru', kegiatanController.getByGuru);
 router.get('/:id', kegiatanController.getById);
