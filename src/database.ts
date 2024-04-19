36704408abde9e3a2de5f97b84b9015ab34d2fe6
louis <louiscaesapdf@gmail.com>
2024-04-19 16:32:40 +0700

    update configurations for vercel

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,8 @@
 import { Pool, PoolConfig } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const dbConfig: PoolConfig = {
   connectionString: "postgres://default:sDMed2HluBV5@ep-old-frost-a179vfkk.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 };
-
-// Change the variable to connect to different database
-// 1. dbConfig for production
-// 2. dbConfigDev for development
 
 const pool = new Pool(dbConfig);
 
