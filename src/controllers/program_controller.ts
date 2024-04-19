90ecc6bf06633773d81008e4c0473ea152fc3393
Putinabillaa <aidiraputiabil@gmail.com>
2024-04-20 01:14:12 +0800

    feat: get prCogram by id more details

--- src/controllers/program_controller.ts
+++ src/controllers/program_controller.ts
@@ -18,10 +18,41 @@ const programController: ProgramController = {
     },
     getById: async (req, res) => {
         try {
-            const { rows } = await postgre.query("SELECT * from program WHERE id_program = $1", [req.params.id]);
-            res.json({msg: "OK", data: rows})
+            const { rows: programs } = await postgre.query("SELECT * from program WHERE id_program = $1", [req.params.id]);
+            const { rows: topik } = await postgre.query(`
+                SELECT t.nama_topik AS title, json_agg(json_build_object('title', k.nama_kegiatan, 'url', '#')) AS activities
+                FROM topik t
+                JOIN kegiatan k ON t.id_topik = k.id_topik
+                WHERE t.id_program = $1
+                GROUP BY t.nama_topik;
+            `, [req.params.id]);
+            const { rows: kompetensi } = await postgre.query(`
+                SELECT k.judul_kompetensi AS title
+                FROM program_kompetensi pk
+                JOIN kompetensi k ON pk.id_kompetensi = k.id_kompetensi
+                WHERE pk.id_program = $1;
+            `, [req.params.id]);
+
+            const result = programs.map(program => ({
+                id: program.id_program,
+                slug: program.nama_program.toLowerCase().replace(/\s+/g, '-'),
+                judul: program.nama_program,
+                imgUrl: `https://images.unsplash.com/photo-1613563696485-f64240817218?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+                tujuan: JSON.parse(program.tujuan_pembelajaran),
+                kompetensi: kompetensi.map(k => k.title),
+                periode: {
+                    tahun_ajaran: program.tahun_akademik,
+                    semester: program.periode_belajar,
+                },
+                topik: topik.map(({ title, activities }) => ({
+                    title,
+                    activities: JSON.parse(activities),
+                })),
+            }));
+
+            res.json({ msg: "OK", data: result });
         } catch (error) {
-            res.json({msg: error.msg})
+            res.json({ msg: error.msg });
         }
     },
     getByGuru: async (req, res) => {
