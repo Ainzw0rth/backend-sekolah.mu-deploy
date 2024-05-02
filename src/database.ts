ec8f9a2060eabf6f964e71fb5d974270f80527b0
Addin Munawwar <moonawar@protonmail.com>
2024-05-02 22:58:07 +0700

    feat: master seeding

--- src/database.ts
+++ src/database.ts
@@ -5,18 +5,18 @@ dotenv.config();
 
 const DB_URL = process.env.POSTGRES_URL;
 const dbConfig: PoolConfig = {
-  connectionString: DB_URL,
+    connectionString: DB_URL,
 };
 
 const pool = new Pool(dbConfig);
 
 pool.connect((err) => {
-  console.log('Connecting to PostgreSQL...');
-  if (err) {
+    console.log('Connecting to PostgreSQL...');
+    if (err) {
     console.error('Error connecting to PostgreSQL:', err);
-  } else {
+    } else {
     console.log('Connected to PostgreSQL successfully!');
-  }
+    }
 });
 
 export default pool;
