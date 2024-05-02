7cda86f60601b1aa0ce8ff4f1919193ea24dae4d
Putinabillaa <aidiraputiabil@gmail.com>
2024-05-02 21:50:22 +0800

    chore: comment put error hasil karya

--- src/routes/hasil_karya.ts
+++ src/routes/hasil_karya.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import multer from 'multer';
-import hasilKaryaController from '../controllers/hasil_karya_controller';
+// import express from 'express';
+// import multer from 'multer';
+// import hasilKaryaController from '../controllers/hasil_karya_controller';
 
-const router = express.Router();
-const upload = multer({ dest: 'uploads/' }); // Define multer middleware to handle file upload
+// const router = express.Router();
+// const upload = multer({ dest: 'uploads/' }); // Define multer middleware to handle file upload
 
-router.get('/:kegiatan_id/:murid_id', hasilKaryaController.getById);
-router.post('/upload', upload.single('file'), hasilKaryaController.uploadFile); // Handle file upload
+// router.get('/:kegiatan_id/:murid_id', hasilKaryaController.getById);
+// router.post('/upload', upload.single('file'), hasilKaryaController.uploadFile); // Handle file upload
 
-export default router;
+// export default router;
