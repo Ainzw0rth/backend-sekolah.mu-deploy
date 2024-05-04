9cf10117415610ec44c6fc52aa1b95a5dc07b9ce
Addin Munawwar <moonawar@protonmail.com>
2024-05-04 20:05:34 +0700

    feat: update presensi kegiatan + log

--- src/controllers/presensi_controller.ts
+++ src/controllers/presensi_controller.ts
@@ -33,25 +33,48 @@ const presensiController: PresensiController = {
         const students = req.body;
         for (const student of students) {
             if (!student.id_murid || !student.catatan_kehadiran) {  
-                res.status(400).json({ msg: 'Invalid request' });
+                res.status(400).json({ msg: 'Invalid request, missing id or catatan_kehadiran' });
                 return;
             }
         }
 
-        for (const student of students) {
-            try {
+        if (!req.query.guru) {
+            res.status(400).json({ msg: 'Invalid request! No id_guru was provided, use ?guru=<id_guru>' });
+            return;
+        }
+
+        const oldStudents = await postgre.query(`
+            SELECT * FROM evaluasi WHERE id_jadwal = $1;
+        `, [req.params.id]);
+        
+        try {
+            await postgre.query(`BEGIN`);
+            for (const student of students) {
+                // Update evaluasi_log as well
+                const timestamp = new Date().toISOString();
+                const editor = req.query.guru;
+                const action = 'Update';
+                const oldValue = oldStudents.rows.find((s: any) => s.id_murid === student.id_murid).catatan_kehadiran;
+                const field = 'catatan_kehadiran';
+
                 await postgre.query(`
                     UPDATE evaluasi
                     SET catatan_kehadiran = $3
-                    WHERE id_kegiatan = $1 AND id_murid = $2;
+                    WHERE id_jadwal = $1 AND id_murid = $2;
                 `, [req.params.id, student.id_murid, student.catatan_kehadiran]);
-                
-            } catch (error) {
-                res.status(500).json({ msg: error.msg });
-                return;
-            }
-        }
 
+
+                await postgre.query(`
+                    INSERT INTO evaluasi_log (id_jadwal, id_murid, timestamp, editor, action, field, old_value)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7);
+                `, [req.params.id, student.id_murid, timestamp, editor, action, field, oldValue]);
+            }         
+            await postgre.query(`COMMIT`);
+        } catch (error) {
+            console.error('Error updating presensi:', error);
+            res.status(500).json({ msg: error.msg });
+            return;
+        }
         res.status(200).json({ msg: 'Data berhasil diubah' });
     }
 }
